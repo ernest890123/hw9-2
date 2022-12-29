ed51414bee7bfa7c031ff2ac47f4f2ba4929fafe
ERNEST DENG <jeremy890123@gmail.com>
2022-12-29 17:49:40 +0800

    1229

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,46 +1,16 @@
-import path from "path";
-
-import express from "express";
-import cors from "cors";
-import WebSocket from "ws";
-
+import express from 'express'; 
+import cors from 'cors';
+import db from './db';
+import dotenv from "dotenv-defaults";
+import mongoose from 'mongoose';
+import routes from './routes';
 const app = express();
-// init middleware
-if (process.env.NODE_ENV === "development") {
-  app.use(cors());
-}
-// define routes
-app.get("/api", (req, res) => {
-  // send the request back to the client
-  console.log("GET /api");
-  res.send({ message: "Hello from the server!" }).status(200);
-});
 
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "../frontend", "build")));
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
-  });
-}
-
-// define server
+app.use(cors());
+app.use(express.json());
+db.connect();
 const port = process.env.PORT || 4000;
-
-const server = app.listen(port, () => {
-  console.log(`Server is up on port ${port}.`);
-});
-
-const wss = new WebSocket.Server({ server });
-
-wss.on("connection", (ws) => {
-  // send a random number every seconds
-  const interval = setInterval(() => {
-    ws.send(JSON.stringify({ number: Math.round(Math.random() * 100) }));
-  }, 1000);
-
-  // clear interval on close
-  ws.on("close", () => {
-    clearInterval(interval);
-  });
-});
+app.use('/', routes);
+app.listen(port, () =>
+ console.log(`Example app listening on port ${port}!`),
+);
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_ROOT =
-  process.env.NODE_ENV === "production"
-    ? "/api"
-    : "http://localhost:4000/api";
+const instance = axios.create({
+  baseURL: `http://localhost:4000/`,
+});
 
-const WS_URL =
-  process.env.NODE_ENV === "production"
-    ? window.location.origin.replace(/^https*/, "ws")
-    : "ws://localhost:4000";
+export default instance;
 
-export const api = axios.create({ baseURL: API_ROOT });
-export const ws = new WebSocket(WS_URL);
+// instance.get('/hi').then((data) => console.log(data));
