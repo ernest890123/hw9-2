30236cfa9a87e4cd4b553f1908bef73de21f5079
ERNEST DENG <jeremy890123@gmail.com>
2022-12-29 22:09:48 +0800

    123312312

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,13 @@ import cors from 'cors';
 import db from './src/db';
 import dotenv from "dotenv-defaults";
 import mongoose from 'mongoose';
-import routes from './routes/routes';
+import routes from './src/routes';
 import path from "path";
 const app = express();
 
 if (process.env.NODE_ENV === "production") {
     const __dirname = path.resolve();
     app.use(express.static(path.join(__dirname, "../frontend", "build")));
-    app.get("/*", function (req, res) {
-      res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
-    });
     db.connect();
     app.use('/', routes);
   }
