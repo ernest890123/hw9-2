972dadb6b4583b61882516fa1aa1c3f55b670e8e
ERNEST DENG <jeremy890123@gmail.com>
2022-12-29 22:23:12 +0800

    try

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const API_ROOT =
-  process.env.NODE_ENV === "production"
-    ? "/"
-    : "http://localhost:4000/";
+const instance = axios.create({
+  baseURL: `/`,
+});
 
-const api = axios.create({ baseURL: API_ROOT })
-export default api;
+export default instance;
