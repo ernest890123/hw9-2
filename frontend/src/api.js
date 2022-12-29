4d695da5712ea6fab08ef988803506ec2a0269a7
ERNEST DENG <jeremy890123@gmail.com>
2022-12-29 21:37:33 +0800

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,5 +4,6 @@ const API_ROOT =
   process.env.NODE_ENV === "production"
     ? "/"
     : "http://localhost:4000/";
+console.log(API_ROOT)
 const api = axios.create({API_ROOT})
 export default api;
