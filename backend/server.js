ccf93dfdc51d30462f35f5bdc7fc3970bc0a123c
ERNEST DENG <jeremy890123@gmail.com>
2022-12-30 12:06:20 +0800

    come

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,6 @@ import routes from './src/routes';
 import path from "path";
 const app = express();
 
-app.use('/', routes);
-db.connect();
-
-if (process.env.NODE_ENV === "development") {
-  app.use(cors());
-}
-
 if (process.env.NODE_ENV === "production") {
     const __dirname = path.resolve();
     app.use(express.static(path.join(__dirname, "../frontend", "build")));
@@ -22,7 +15,13 @@ if (process.env.NODE_ENV === "production") {
     });
   }
 
+app.use(cors());
+app.use(express.json());
+app.use('/api', routes);
+db.connect();
+
 const port = process.env.PORT || 4000;
 app.listen(port, () =>
  console.log(`Example app listening on port ${port}!`),
 );
+
