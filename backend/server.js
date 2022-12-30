b9b9d2ad240440c5a0c1076ad9617d561e393b77
ERNEST DENG <jeremy890123@gmail.com>
2022-12-30 12:23:59 +0800

    234

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 if (process.env.NODE_ENV === "production") {
     const __dirname = path.resolve();
     app.use(express.static(path.join(__dirname, "../frontend", "build")));
-    app.get("/*", function (req, res) {
+    app.get("/", function (req, res) {
       res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
     });
   }
